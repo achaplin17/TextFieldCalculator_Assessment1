7ed9dc727d93e4931309fd5efd41a234c546a61d
Anders Chaplin <achaplin17@cmc.edu>
2015-05-15 13:16:20 -0700

    firstcommit

---
+++ TextFieldCalculator_Assessment1/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  TextFieldCalculator_Assessment1
+//
+//  Created by Anders Chaplin on 5/15/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ TextFieldCalculator_Assessment1/WebViewController.m
@@ -0,0 +1,37 @@
+//
+//  WebViewController.m
+//  TextFieldCalculator_Assessment1
+//
+//  Created by Anders Chaplin on 5/15/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
