c9e5932a22968af298779f40efc2eb3ae3996ebf
Anders Chaplin <achaplin17@cmc.edu>
2015-05-15 09:11:51 -0700

    Initial Commit

---
+++ TextFieldCalculator_Assessment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFieldCalculator_Assessment1
+//
+//  Created by Anders Chaplin on 5/15/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextFieldCalculator_Assessment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextFieldCalculator_Assessment1
+//
+//  Created by Anders Chaplin on 5/15/15.
+//  Copyright (c) 2015 ___AndersChaplin___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
